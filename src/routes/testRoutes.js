5bafdd2f9aee35ad7938f1ce5a1ad6e622b1572f
Ahmad Bin Younas <abyrulzzzz091@gmail.com>
2026-06-05 21:50:47 +0500

    chore(test): remove test controller and routes
    - Delete testController.js with sendTestNotification endpoint
    - Delete testRoutes.js with test notification route
    - Clean up debug/development test utilities no longer needed

--- src/routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { sendTestNotification } = require('../controllers/testController');
-const { protect } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.post('/send-notification', protect, sendTestNotification);
-
-module.exports = router;
